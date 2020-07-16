d2704b4ec1c65ce0896d4495bf1f397ede58298b
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2020-07-15 22:43:16 -0700

    Remove original iso target, create arm64 and amd64 efi capable isos

--- deploy/iso/minikube-iso/board/coreos-arm64/minikube/post-image.sh
+++ deploy/iso/minikube-iso/board/coreos-arm64/minikube/post-image.sh
@@ -3,8 +3,26 @@
 set -e
 
 BOARD_DIR="$(dirname $0)"
+ROOTFS_TMP_TARGET_DIR=`mktemp -d`
 
-UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.ext2" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
-sed -i "s/UUID_TMP/$UUID/g" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
-sed "s/UUID_TMP/$UUID/g" $BOARD_DIR/genimage-efi.cfg > "$BINARIES_DIR/genimage-efi.cfg"
-support/scripts/genimage.sh -c "$BINARIES_DIR/genimage-efi.cfg"
+# Create EFI Bootloader Partition vfat image
+support/scripts/genimage.sh -c "${BINARIES_DIR}/genimage-efi.cfg"
+
+# Copy Grub2 EFI Config
+mkdir -p ${ROOTFS_TMP_TARGET_DIR}/EFI/BOOT
+cp ${BINARIES_DIR}/efi-part/EFI/BOOT/bootaa64.efi  ${ROOTFS_TMP_TARGET_DIR}/EFI/BOOT/bootaa64.efi
+cp ${BOARD_DIR}/grub.cfg ${ROOTFS_TMP_TARGET_DIR}/EFI/BOOT/grub.cfg
+
+# Copy Kernal and Initramfs
+cp ${BINARIES_DIR}/Image ${ROOTFS_TMP_TARGET_DIR}/Image
+cp ${BINARIES_DIR}/rootfs.cpio.gz ${ROOTFS_TMP_TARGET_DIR}/initrd
+
+# Create CD Image
+xorriso -as mkisofs -V 'MINIKUBE' -o ${BINARIES_DIR}/rootfs.iso9660 \
+-J -R -no-emul-boot \
+-e --interval:appended_partition_2:all:: \
+-append_partition 2 0xef ${BINARIES_DIR}/grub-efi.img \
+${ROOTFS_TMP_TARGET_DIR}
+
+# Cleanup
+rm -rf ${ROOTFS_TMP_TARGET_DIR}
