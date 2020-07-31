8b666229868ad3a9245da376b2cae9497b6886cb
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2020-07-31 07:46:49 -0700

    ARM64

--- deploy/iso/minikube-iso/package/containerd-bin/containerd-bin.mk
+++ deploy/iso/minikube-iso/package/containerd-bin/containerd-bin.mk
@@ -8,29 +8,20 @@ CONTAINERD_BIN_COMMIT = 8fba4e9a7d01810a393d5d25a3621dc101981175
 CONTAINERD_BIN_SITE = https://github.com/containerd/containerd/archive
 CONTAINERD_BIN_SOURCE = $(CONTAINERD_BIN_VERSION).tar.gz
 CONTAINERD_BIN_DEPENDENCIES = host-go libgpgme
-CONTAINERD_BIN_GOPATH = $(@D)/_output
-CONTAINERD_BIN_ENV = \
-	$(GO_TARGET_ENV) \
+CONTAINERD_BIN_GO_ENV = \
 	CGO_ENABLED=1 \
 	GO111MODULE=off \
-	GOPATH="$(CONTAINERD_BIN_GOPATH)" \
-	GOBIN="$(CONTAINERD_BIN_GOPATH)/bin" \
-	PATH=$(CONTAINERD_BIN_GOPATH)/bin:$(BR_PATH)
-
-CONTAINERD_BIN_COMPILE_SRC = $(CONTAINERD_BIN_GOPATH)/src/github.com/containerd/containerd
+	GOPATH=$(@D)/$(CONTAINERD_BIN_WORKSPACE) \
+	GOBIN="$(@D)/$(CONTAINERD_BIN_WORKSPACE)/bin" \
+	PATH=$(@D)/$(CONTAINERD_BIN_WORKSPACE)/bin:$(BR_PATH)
 
 define CONTAINERD_BIN_USERS
 	- -1 containerd-admin -1 - - - - -
 	- -1 containerd       -1 - - - - -
 endef
 
-define CONTAINERD_BIN_CONFIGURE_CMDS
-	mkdir -p $(CONTAINERD_BIN_GOPATH)/src/github.com/containerd
-	ln -sf $(@D) $(CONTAINERD_BIN_COMPILE_SRC)
-endef
-
 define CONTAINERD_BIN_BUILD_CMDS
-	PWD=$(CONTAINERD_BIN_COMPILE_SRC) $(CONTAINERD_BIN_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) VERSION=$(CONTAINERD_BIN_VERSION) REVISION=$(CONTAINERD_BIN_COMMIT) -C $(@D) binaries
+	PWD=$(CONTAINERD_BIN_SRC_PATH) $(GO_TARGET_ENV) $(CONTAINERD_BIN_GO_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) VERSION=$(CONTAINERD_BIN_VERSION) REVISION=$(CONTAINERD_BIN_COMMIT) -C $(@D) binaries
 endef
 
 define CONTAINERD_BIN_INSTALL_TARGET_CMDS
@@ -39,6 +30,12 @@ define CONTAINERD_BIN_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/usr/bin
 	$(INSTALL) -Dm755 \
 		$(@D)/bin/containerd-shim \
+		$(TARGET_DIR)/usr/bin
+	$(INSTALL) -Dm755 \
+		$(@D)/bin/containerd-shim-runc-v1 \
+		$(TARGET_DIR)/usr/bin
+	$(INSTALL) -Dm755 \
+		$(@D)/bin/containerd-shim-runc-v2 \
 		$(TARGET_DIR)/usr/bin
 	$(INSTALL) -Dm755 \
 		$(@D)/bin/ctr \
@@ -57,4 +54,4 @@ define CONTAINERD_BIN_INSTALL_INIT_SYSTEMD
 		$(TARGET_DIR)/usr/lib/systemd/system-preset/50-minikube.preset
 endef
 
-$(eval $(generic-package))
+$(eval $(golang-package))
--- deploy/iso/minikube-iso/package/docker-amd64-bin/docker-amd64-bin.mk
+++ deploy/iso/minikube-iso/package/docker-amd64-bin/docker-amd64-bin.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 
-DOCKER_BIN_VERSION = 19.03.13
-DOCKER_BIN_SITE = https://download.docker.com/linux/static/stable/x86_64
-DOCKER_BIN_SOURCE = docker-$(DOCKER_BIN_VERSION).tgz
+DOCKER_AMD64_BIN_VERSION = 19.03.13
+DOCKER_AMD64_BIN_SITE = https://download.docker.com/linux/static/stable/x86_64
+DOCKER_AMD64_BIN_SOURCE = docker-$(DOCKER_AMD64_BIN_VERSION).tgz
 
-define DOCKER_BIN_USERS
+define DOCKER_AMD64_BIN_USERS
 	- -1 docker -1 - - - - -
 endef
 
-define DOCKER_BIN_INSTALL_TARGET_CMDS
+define DOCKER_AMD64_BIN_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 \
 		$(@D)/docker \
 		$(TARGET_DIR)/bin/docker
@@ -38,17 +38,17 @@ define DOCKER_BIN_INSTALL_TARGET_CMDS
 	# https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
 
 	$(INSTALL) -Dm644 \
-		$(DOCKER_BIN_PKGDIR)/daemon.json \
+		$(DOCKER_AMD64_BIN_PKGDIR)/daemon.json \
 		$(TARGET_DIR)/etc/docker/daemon.json
 endef
 
-define DOCKER_BIN_INSTALL_INIT_SYSTEMD
+define DOCKER_AMD64_BIN_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 \
-		$(BR2_EXTERNAL)/package/docker-bin/docker.socket \
+		$(DOCKER_ARM64_BIN_PKGDIR)/docker.socket \
 		$(TARGET_DIR)/usr/lib/systemd/system/docker.socket
 
 	$(INSTALL) -D -m 644 \
-		$(DOCKER_BIN_PKGDIR)/forward.conf \
+		$(DOCKER_AMD64_BIN_PKGDIR)/forward.conf \
 		$(TARGET_DIR)/etc/sysctl.d/forward.conf
 endef
 
