b0cae1d7e6e35c690573db6d607cbf8bb1e4f180
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2020-07-31 19:41:44 -0700

    Fixup AMD64 EFI boot and convert to pure xorriso format

--- deploy/iso/minikube-iso/board/coreos-arm64/minikube/post-image.sh
+++ deploy/iso/minikube-iso/board/coreos-arm64/minikube/post-image.sh
@@ -13,16 +13,24 @@ mkdir -p ${ROOTFS_TMP_TARGET_DIR}/EFI/BOOT
 cp ${BINARIES_DIR}/efi-part/EFI/BOOT/bootaa64.efi  ${ROOTFS_TMP_TARGET_DIR}/EFI/BOOT/bootaa64.efi
 cp ${BOARD_DIR}/grub.cfg ${ROOTFS_TMP_TARGET_DIR}/EFI/BOOT/grub.cfg
 
-# Copy Kernal and Initramfs
+# Copy Kernel and Initramfs
 cp ${BINARIES_DIR}/Image ${ROOTFS_TMP_TARGET_DIR}/Image
 cp ${BINARIES_DIR}/rootfs.cpio.gz ${ROOTFS_TMP_TARGET_DIR}/initrd
 
 # Create CD Image
-xorriso -as mkisofs -V 'MINIKUBE' -o ${BINARIES_DIR}/rootfs.iso9660 \
--J -R -no-emul-boot \
--e --interval:appended_partition_2:all:: \
--append_partition 2 0xef ${BINARIES_DIR}/grub-efi.img \
-${ROOTFS_TMP_TARGET_DIR}
+rm -f ${BINARIES_DIR}/rootfs.iso9660
+xorriso -volid 'MINIKUBE' \
+  -outdev ${BINARIES_DIR}/rootfs.iso9660 \
+  -padding 0 \
+  -map ${ROOTFS_TMP_TARGET_DIR} / \
+  -chmod 0755 / -- \
+  -boot_image any iso_mbr_part_type=0x83 \
+  -boot_image any cat_path='/boot.catalog' \
+  -boot_image any appended_part_as=gpt \
+  -boot_image any emul_type=no_emulation \
+  -append_partition 1 0xee ${BINARIES_DIR}/grub-efi.img \
+  -boot_image grub efi_path=--interval:appended_partition_1:all::
+
 
 # Cleanup
 rm -rf ${ROOTFS_TMP_TARGET_DIR}
