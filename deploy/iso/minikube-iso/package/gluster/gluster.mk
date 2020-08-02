3cb7d2258748e33690bf4596ff85351ffc325495
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2020-08-01 18:19:53 -0700

    Upgrade Gluster Client to version 8.0

--- deploy/iso/minikube-iso/package/gluster/gluster.mk
+++ deploy/iso/minikube-iso/package/gluster/gluster.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-GLUSTER_VERSION = 7.6
-GLUSTER_SITE = https://download.gluster.org/pub/gluster/glusterfs/7/$(GLUSTER_VERSION)
+GLUSTER_VERSION = 8.0
+GLUSTER_SITE = https://download.gluster.org/pub/gluster/glusterfs/$(shell echo $(GLUSTER_VERSION) | cut -d. -f1)/$(GLUSTER_VERSION)
 GLUSTER_SOURCE = glusterfs-$(GLUSTER_VERSION).tar.gz
-GLUSTER_CONF_OPTS = --disable-ec-dynamic --disable-georeplication --disable-ibverbs --disable-gnfs --disable-cmocka --without-server
+GLUSTER_CONF_OPTS = --disable-ec-dynamic --disable-georeplication --disable-gnfs --disable-cmocka --without-server
 GLUSTER_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) install
 $(eval $(autotools-package))
