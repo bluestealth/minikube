a823e29fece3f36022c81bcc9ed14804993bb32c
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2020-10-26 14:01:39 -0700

    update falco to 0.26.1

--- deploy/iso/minikube-iso/package/falco-module/falco-module.mk
+++ deploy/iso/minikube-iso/package/falco-module/falco-module.mk
@@ -4,7 +4,7 @@
 #
 ########################################################################
 
-FALCO_MODULE_VERSION = 0.24.0
+FALCO_MODULE_VERSION = 0.26.1
 FALCO_MODULE_SITE = https://github.com/falcosecurity/falco/archive
 FALCO_MODULE_SOURCE = $(FALCO_MODULE_VERSION).tar.gz
 FALCO_MODULE_DEPENDENCIES += ncurses libyaml
@@ -12,7 +12,7 @@ FALCO_MODULE_LICENSE = Apache-2.0
 FALCO_MODULE_LICENSE_FILES = COPYING
 
 # see cmake/modules/sysdig-repo/CMakeLists.txt
-FALCO_MODULE_SYSDIG_VERSION = 85c88952b018fdbce2464222c3303229f5bfcfad
+FALCO_MODULE_SYSDIG_VERSION = 2aa88dcf6243982697811df4c1b484bcbe9488a2
 FALCO_MODULE_EXTRA_DOWNLOADS = https://github.com/draios/sysdig/archive/${FALCO_MODULE_SYSDIG_VERSION}.tar.gz
 
 define FALCO_MODULE_SYSDIG_SRC
