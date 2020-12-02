f12fa53eaf3c33aabf6d0c6d6fbe8a37841be08e
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2020-12-01 20:20:30 -0800

    Update crun to 0.16

--- deploy/iso/minikube-iso/package/crun/crun.mk
+++ deploy/iso/minikube-iso/package/crun/crun.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-CRUN_VERSION = 0.15
-CRUN_COMMIT = 56ca95e61639510c7dbd39ff512f80f626404969
+CRUN_VERSION = 0.16
+CRUN_COMMIT = 91ec195708efb8eed1699d59cd0369d639d8a7a8
 CRUN_LIBOCISPEC_COMMIT = 5dfe2f406dc2d0f244aec621292e4e0a52149240
 CRUN_SITE = https://github.com/containers/crun/releases/download/$(CRUN_VERSION)
 CRUN_SOURCE = crun-$(CRUN_VERSION).tar.xz
