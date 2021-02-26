9c493673095a04af836ad4d079ac07d287865fdf
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2021-02-26 12:28:36 -0800

    Update runc to v1.0.0-rc93

--- deploy/iso/minikube-iso/package/runc-master/runc-master.mk
+++ deploy/iso/minikube-iso/package/runc-master/runc-master.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-# As of 2020-08-05, v1.0.0-rc92
+# As of 2020-02-26, v1.0.0-rc93
 RUNC_MASTER_SITE = https://github.com/opencontainers/runc/archive
-RUNC_MASTER_VERSION = v1.0.0-rc92
-RUNC_MASTER_COMMIT = ff819c7e9184c13b7c2607fe6c30ae19403a7aff
+RUNC_MASTER_VERSION = v1.0.0-rc93
+RUNC_MASTER_COMMIT = 12644e614e25b05da6fd08a38ffa0cfe1903fdec
 RUNC_MASTER_SOURCE = $(RUNC_MASTER_VERSION).tar.gz
 RUNC_MASTER_LICENSE = Apache-2.0
 RUNC_MASTER_LICENSE_FILES = LICENSE
